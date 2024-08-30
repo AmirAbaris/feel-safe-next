f3f824d34ee32626a2eb5348e5afcf8c3d9a2459
Amir Zarei <118467205+AmirAbaris@users.noreply.github.com>
2024-08-30 23:43:42 +0330

    [UPDATE] hero section implementation

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,9 +1,11 @@
+import HeroSection from '@/components/HeroSection';
 import { useTranslations } from 'next-intl';
 
 export default function HomePage() {
     const t = useTranslations('HomePage');
     return (
-        <div>
+        <div className='flex h-screen items-center justify-center'>
+            <HeroSection />
         </div>
     );
 }
