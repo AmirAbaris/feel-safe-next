4eb2832433b41ae8c59f98fc34222303099aedce
Amir Zarei <118467205+AmirAbaris@users.noreply.github.com>
2024-08-31 00:39:22 +0330

    [UPDATE] added init for lucia and mongodb

---
+++ lib/auth.ts
@@ -0,0 +1,21 @@
+import { MongodbAdapter } from "@lucia-auth/adapter-mongodb";
+import { Lucia } from "lucia";
+import { Session, User } from "./db";
+
+const adapter = new MongodbAdapter(Session, User); // your adapter
+
+export const lucia = new Lucia(adapter, {
+    sessionCookie: {
+        attributes: {
+            // set to `true` when using HTTPS
+            secure: process.env.NODE_ENV === "production"
+        }
+    }
+});
+
+// IMPORTANT!
+declare module "lucia" {
+    interface Register {
+        Lucia: typeof lucia;
+    }
+}
